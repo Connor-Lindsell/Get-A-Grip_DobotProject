eb9fd98ec991b4c1ebcf0d932b570211f2a45e9c
Connor Lindsell <connor.lindsell@student.uts.edu.au>
2025-10-29 13:33:05 +1100

    calibrated

--- Get_A_Grip/+perception/getCameraCalibration.m
+++ Get_A_Grip/+perception/getCameraCalibration.m
@@ -1,49 +1,37 @@
 function camParam = getCameraCalibration()
-%GETCAMERACALIBRATION  Return intrinsics/distortion from Bouguet-style results.
-% Returns:
-%   camParam.intr      : cameraIntrinsics object
-%   camParam.K         : 3x3 intrinsic matrix (Computer Vision Toolbox convention)
-%   camParam.kc        : [k1 k2 p1 p2 k3] Bouguet order
-%   camParam.radial    : [k1 k2 k3]
-%   camParam.tangential: [p1 p2]
-%   camParam.imageSize : [ny nx]
-%   camParam.focal     : [fx fy]
-%   camParam.principal : [cx cy]
-%   camParam.skew      : alpha_c
+%GETCAMERACALIBRATION  Return intrinsics/distortion from Bouguet calibration results.
+% Copy fc, cc, alpha_c, kc, nx, ny directly from Calib_Results.m
 
-    % ==== Paste from Matlab Camera Calibration toolbox results ====
-    fx = 692.609244167284487;
-    fy = 690.065254803508310;
+    % ==== Paste from Calib_Results.m ====
+    fc = [ 907.016155766797283 ; 912.211105886656696 ];
+    cc = [ 660.156166957534424 ; 384.191277635435142 ];
+    alpha_c = 0.000000000000000;
+    kc = [ 0.167298999562626 ; -0.354312559662885 ; ...
+           -0.003249389155066 ; 0.002937533008856 ; 0.000000000000000 ];
+    nx = 1280;
+    ny = 720;
+    % ====================================
 
-    cx = 284.905512041129725;
-    cy = 244.626846325448014;
+    % Assign clearer names for readability
+    fx = fc(1);
+    fy = fc(2);
+    cx = cc(1);
+    cy = cc(2);
 
-    alpha_c = 0.0; % skew
- 
-    % Bouguet kc = [k1 k2 p1 p2 k3]
-    kc = [ 0.106697069970503 ; -0.250539104052457 ; ...
-           0.000104056976962 ; -0.000248328870229 ; 0.0 ];
+    % Split distortion terms
+    radial     = [kc(1) kc(2) kc(5)];  % [k1 k2 k3]
+    tangential = [kc(3) kc(4)];        % [p1 p2]
 
-    nx = 1280; % width
-    ny = 720;  % height
-    % ========================================
-
-    % Split distortion in MATLAB CVT order
-    radial      = [kc(1) kc(2) kc(5)]; % [k1 k2 k3]
-    tangential  = [kc(3) kc(4)];       % [p1 p2]
-
-    % Construct cameraIntrinsics (preferred, lightweight and accepted by most CVT funcs)
+    % Construct cameraIntrinsics object
     intr = cameraIntrinsics([fx fy], [cx cy], [ny nx], ...
         'Skew', alpha_c, ...
         'RadialDistortion', radial, ...
         'TangentialDistortion', tangential);
 
-    % MATLAB stores IntrinsicMatrix as:
-    % [fx  0   0
-    %  0  fy   0
-    %  cx cy   1]
+    % Build intrinsic matrix (3x3)
     K = intr.IntrinsicMatrix';
 
+    % Output struct
     camParam = struct( ...
         'intr', intr, ...
         'K', K, ...
@@ -54,5 +42,5 @@ function camParam = getCameraCalibration()
         'focal', [fx fy], ...
         'principal', [cx cy], ...
         'skew', alpha_c );
-
 end
+
